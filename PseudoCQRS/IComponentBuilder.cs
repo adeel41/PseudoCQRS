fc80e5e7ffc05278ce178f1e96956b0513389d84
Adeel <adeel41@yahoo.com>
2014-10-29 10:40:44 +0000

    changed AddValidator by adding optional parameter 'onFailure' and added RedirectToRoute method in ComponentResultHelper

--- PseudoCQRS/IComponentBuilder.cs
+++ PseudoCQRS/IComponentBuilder.cs
@@ -12,20 +12,24 @@ namespace PseudoCQRS
 		IComponentBuilder ViewModelProvider<TViewModel>( Func<TViewModel> viewModelProviderFor );
 		IComponentBuilder CommandHandler<TCommand>( Func<TCommand> commandHandlerFor );
 		IComponentBuilder AccessChecker<T, TAccessChecker>( Func<T, object> property ) where TAccessChecker : IAccessChecker;
-		IComponentBuilder AddValidator<T, TValidationChecker>() where TValidationChecker : IValidationChecker<T>;
+		IComponentBuilder AddValidator<T, TValidationChecker>(Func<ComponentResultHelper, IComponentResult> onfailure = null ) where TValidationChecker : IValidationChecker<T>;
 		IComponentBuilder OnSuccess( Func<ComponentResultHelper, IComponentResult> onSuccessFunction );
 		IComponentBuilder AuthorizedTo<TAuthorizationChecker>() where TAuthorizationChecker : IAuthorizationChecker;
 	}
 
 	public class ComponentResultHelper
 	{
-		// these functions are just proxy calls, they call implementations which can be in mvc project or somewhere else.
 		public IComponentResult RedirectTo( string url )
 		{
 			throw new NotImplementedException();
 		}
 
-		public IComponentResult View( string path )   // don't like the name
+		public IComponentResult RedirectToRoute( Func<ComponentRouteBuilder, string> routeBuilder )
+		{
+			throw new NotImplementedException();
+		}
+
+		public IComponentResult View( string path )
 		{
 			throw new NotImplementedException();
 		}
@@ -34,6 +38,10 @@ namespace PseudoCQRS
 		{
 			throw new NotImplementedException();
 		}
+	}
+
+	public class ComponentRouteBuilder
+	{
 	}
 
 
